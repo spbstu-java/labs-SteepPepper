1378205f01a6d6d192a941ebfc02b29a20b73358
SteepPepper <GreenRun@list.ru>
2022-11-06 17:45:43 +0300

    Lab_2 Complete

---
+++ ExceptionMass/DivideByZeroException.java
@@ -0,0 +1,8 @@
+package LAB2.ExceptionMass;
+
+public class DivideByZeroException extends ArithmeticException{
+    public DivideByZeroException(String message) {
+        super(message);
+    }
+
+}
